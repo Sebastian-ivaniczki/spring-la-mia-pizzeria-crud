f49e0838ceb1d1264b971652653dc50e503ccfad
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-05-29 15:06:00 +0200

    create method add

--- src/main/java/group/java/demo/controller/ControllerPizza.java
+++ src/main/java/group/java/demo/controller/ControllerPizza.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -56,6 +57,24 @@ public class ControllerPizza {
         
         return "singlepizza";
     }
+	
+	
+	@GetMapping("pizzas/create")
+	public String createPizza() {
+		
+		return "create"; 
+		
+	}
+	
+	
+	@PostMapping("pizzas/create")
+	public String storePizza(@ModelAttribute Pizza pizza) {
+		
+		pizzaService.save(pizza);
+		
+		return "redirect:/";
+		
+	}
 	
 
 }
--- src/main/java/group/java/demo/service/PizzaService.java
+++ src/main/java/group/java/demo/service/PizzaService.java
@@ -38,4 +38,6 @@ public class PizzaService {
 		return pizzaRepo.findByNameContaining(name);
 		
 	}
+	
+
 }
